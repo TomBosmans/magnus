8a6e8215c6759da69596f96248affb227d564d67
TomBosmans <t.bosse@hotmail.com>
2018-04-29 21:45:18 +0200

    restructured rails app
    use public and admin instead of application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def namespace
+    controller_path.split('/').first
+  end
+
   def component(component_name, locals = {}, &block)
     name = component_name.to_s.split('_').first
     render "components/#{name}/#{component_name}", locals, &block
---
+++ frontend/packs/admin.js
@@ -0,0 +1,1 @@
+import "init/admin_init";
