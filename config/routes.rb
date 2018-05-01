945acb1799a5355949330ccea6c5fcacd492de41
TomBosmans <t.bosse@hotmail.com>
2018-05-01 15:49:30 +0200

    created public scope in routes
    controllers will be in app/controllers/public.
    but the uri will not include 'public'

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :dashboard, only: %w(index)
   end
+
+  # Everything in here uses controller public/*
+  # but the url does not include public
+  scope module: :public do; end
 end
