5193c3ce4474d7a959fb3249b635242eb3d16e31
TomBosmans <t.bosse@hotmail.com>
2018-05-01 15:49:06 +0200

    added namespace to component generation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,6 @@ module ApplicationHelper
 
   def component(component_name, locals = {}, &block)
     name = component_name.to_s.split('_').first
-    render "components/#{name}/#{component_name}", locals, &block
+    render "components/#{namespace}/#{name}/#{component_name}", locals, &block
   end
 end
--- lib/generators/component_generator.rb
+++ lib/generators/component_generator.rb
@@ -1,5 +1,5 @@
 class ComponentGenerator < Rails::Generators::Base
-  argument :component_name, required: true, desc: 'Component name, e.g: button'
+  argument :name, required: true, desc: 'Component name, e.g: button'
 
   def create_view_file
     create_file "#{component_path}/_#{component_name}.html.erb"
@@ -19,6 +19,26 @@ class ComponentGenerator < Rails::Generators::Base
   protected
 
   def component_path
-    "frontend/components/#{component_name}"
+    if namespace?
+      "frontend/components/#{namespace}/#{component_name}"
+    else
+      "frontend/components/#{component_name}"
+    end
+  end
+
+  def split_name
+    name.split(/[:,::,\/]/).reject(&:blank?).map(&:underscore)
+  end
+
+  def component_name
+    split_name.last.underscore
+  end
+
+  def namespace
+    split_name[0...-1].join("_")
+  end
+
+  def namespace?
+    split_name.size > 1
   end
 end
