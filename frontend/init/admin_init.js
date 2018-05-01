9f73db867f223b9ae4ab1a19d18d87edeb366405
TomBosmans <t.bosse@hotmail.com>
2018-05-01 16:27:30 +0200

    turbolinks, rails-ujs and jquery added

--- frontend/init/admin_init.js
+++ frontend/init/admin_init.js
@@ -1,1 +1,10 @@
+import Rails from "rails-ujs";
+import Turbolinks from "turbolinks";
+
+import "materialize-css/dist/css/materialize.css";
+import "materialize-css/dist/js/materialize";
+import "material-design-icons/iconfont/material-icons.css";
 import "./admin_init.css";
+
+Rails.start();
+Turbolinks.start();
