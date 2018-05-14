d90d088fc1c04b04391871cd90466ce828204876
TomBosmans <t.bosse@hotmail.com>
2018-05-14 20:07:31 +0200

    import materialize manual and set primary/secondary variables

--- frontend/init/admin_init.js
+++ frontend/init/admin_init.js
@@ -1,10 +1,9 @@
 import Rails from "rails-ujs";
 // import Turbolinks from "turbolinks";
 
-import "materialize-css/dist/css/materialize.css";
 import "materialize-css/dist/js/materialize";
 import "material-design-icons/iconfont/material-icons.css";
-import "./admin_init.css";
+import "./admin_init.scss";
 
 Rails.start();
 // Turbolinks.start();
