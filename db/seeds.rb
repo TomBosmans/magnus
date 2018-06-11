e0e0251bf6be7f197545d0d2d48617616ac826b2
TomBosmans <t.bosse@hotmail.com>
2018-06-11 15:35:25 +0200

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,15 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
+Tenant.create(
+  name: 'Magnus',
+  description: 'Example tenant',
+  subdomain: 'www.magnus'
+)
+
 User.create(
   email: 'admin@magnus.com',
   password: 'magnusmagnus'
-)
-
-Setting.create(
-  name: 'Magnus',
-  description: 'Back end for your front end'
 )
 
 content = Group.create(
